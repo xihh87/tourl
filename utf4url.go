b16f1303f0570de79a254bf47233d13928a2f218
Joshua Haase <hahj87@gmail.com>
2016-06-20 12:00:21 -0500

    Use UTF-8 const from utf8 package.

--- utf4url.go
+++ utf4url.go
@@ -9,26 +9,18 @@ import (
 	"unicode/utf8"
 )
 
-// The UTF constants from https://golang.org/pkg/unicode/utf8/
-const (
-	RuneError = '\uFFFD'     // the "error" Rune or "Unicode replacement character"
-	RuneSelf  = 0x80         // characters below Runeself are represented as themselves in a single byte.
-	MaxRune   = '\U0010FFFF' // Maximum valid Unicode code point.
-	UTFMax    = 4            // maximum number of bytes of a UTF-8 encoded Unicode character.
-)
-
 func main() {
 	rd := bufio.NewReader(os.Stdin)
 	wt := io.Writer(os.Stdout)
 	s := 0
-	buf := make([]byte, UTFMax)
+	buf := make([]byte, utf8.UTFMax)
 
 	for c, _, err := rd.ReadRune(); err == nil; c, _, err = rd.ReadRune() {
 		s = utf8.EncodeRune(buf, c)
 		if !utf8.ValidRune(c) {
 			continue
 		}
-		if c < RuneSelf {
+		if c < utf8.RuneSelf {
 			wt.Write(buf[:s])
 		} else {
 			wt.Write([]byte(template.URLQueryEscaper(string(buf[:s]))))
